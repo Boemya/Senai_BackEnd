b90a61e443ca3e6c059db2163dbd4ede7b1f48ad
Emely Rocha <100489596+Boemya@users.noreply.github.com>
2023-04-06 15:38:09 -0300

    Add files via upload

---
+++ javax/app.js
@@ -0,0 +1,14 @@
+let n1 = 45
+
+if(n1<16){
+    console.log("Não vota")
+}
+else if(n1>65){
+    console.log("Voto NÃO Obrigatório")
+}
+else if(n1>=16 && n1<18){
+    console.log("Voto NÃO Obrigatório")
+}
+else{
+    console.log("VOTO OBRIGATÓRIO!!!")
+}
